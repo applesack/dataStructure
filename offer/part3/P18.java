5790b7e4617a04cfe19637219abb8e0ce93284eb
applesack <2383955734@qq.com>
2020-08-04 21:02:36 +0800

    8/04 晚

---
+++ offer/part3/P18.java
@@ -0,0 +1,17 @@
+package part3;
+
+/**
+ * @author : flutterdash@qq.com
+ * @date : 2020年08月04日 19:32
+ */
+public class P18 {
+
+    private void deleteNode(LinkNode pListHead, LinkNode pToDelete) {
+
+    }
+
+    private class LinkNode {
+        int value;
+        LinkNode next;
+    }
+}
