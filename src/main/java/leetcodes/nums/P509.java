2611f80de3077a919de0bebc52fdce7aadab3afe
applesack <2383955734@qq.com>
2020-09-16 10:59:31 +0800

    20/9/16

---
+++ src/main/java/leetcodes/nums/P509.java
@@ -0,0 +1,31 @@
+package leetcodes.nums;
+
+import org.junit.Test;
+
+/**
+ * @author : flutterdash@qq.com
+ * @date : 2020年09月15日 10:37
+ */
+public class P509 {
+
+    @Test
+    public void testFunc() {
+        System.out.println(fib(5));
+    }
+
+    public int fib(int N) {
+        if (N <= 0)
+            return 0;
+        if (N <= 2)
+            return 1;
+        int pre = 1, cur = 1, sum;
+        for (int i = 2; i<N; i++) {
+            sum = pre + cur;
+            pre = cur;
+            cur = sum;
+        }
+
+        return cur;
+    }
+
+}
